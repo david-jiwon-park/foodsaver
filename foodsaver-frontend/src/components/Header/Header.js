1861fc7c7ff0281088f3a37ccea33ee15796b04a
david-jiwon-park <davidpark48@hotmail.com>
2024-03-15 04:56:31 -0400

    Updating nav so that it will close if the link matches the current location

--- foodsaver-frontend/src/components/Header/Header.js
+++ foodsaver-frontend/src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import './Header.scss'
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import navIcon from '../../assets/icons/nav-icon.png'
 import Nav from '../Nav/Nav';
 
@@ -14,17 +15,25 @@ const Header = ({ setIsLoggedIn }) => {
     }
   };
 
+  const location = useLocation();
+  const currentLocation = location.pathname;
+
+  const closeNavIfLocationMatches = (paths) => {
+    if (paths.some(path => currentLocation.startsWith(path))) {
+      setIsNavOpen(false);
+    }
+  }
+
   return(
     <div className="header">
       <h1 className="header__heading">Food Saver</h1>
       <img className="header__nav-icon" src={navIcon} alt='nav icon' onClick={() => toggleNav()}/>
  
-      
       <Nav 
         isOpen={isNavOpen} 
+        closeNavIfLocationMatches={closeNavIfLocationMatches}
         setIsLoggedIn={setIsLoggedIn} 
       />
-
 
     </div>
 
--- foodsaver-frontend/src/components/Nav/Nav.js
+++ foodsaver-frontend/src/components/Nav/Nav.js
@@ -1,23 +1,24 @@
 import './Nav.scss'
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Nav = ({ isOpen }) => {
+const Nav = ({ isOpen, closeNavIfLocationMatches }) => {
   if (!isOpen) return null;
-  
+
+  const pathsToCheck = ["/inventory", "/recipes", "/favorites", "/profile"];
 
   return(
     <div className="nav">
       <div className="nav__container">
-        <Link className="nav__link" to="/inventory">
+        <Link className="nav__link" to="/inventory" onClick={() => closeNavIfLocationMatches(pathsToCheck)}>
           <button className='nav__button'>Inventory</button>
         </Link>
-        <Link className="nav__link" to="/recipes">
+        <Link className="nav__link" to="/recipes" onClick={() => closeNavIfLocationMatches(pathsToCheck)}>
           <button className='nav__button'>Recipes</button>
         </Link>
-        <Link className="nav__link" to="/favorites">
+        <Link className="nav__link" to="/favorites" onClick={() => closeNavIfLocationMatches(pathsToCheck)}>
           <button className='nav__button'>Favorites</button>
         </Link>
-        <Link className="nav__link" to="/profile">
+        <Link className="nav__link" to="/profile" onClick={() => closeNavIfLocationMatches(pathsToCheck)}>
           <button className='nav__button'>User Profile</button>
         </Link>
       </div>
