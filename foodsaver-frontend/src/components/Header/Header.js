a9290b50bd2dcb2bd50395fdfba1fcc8b7a46aeb
david-jiwon-park <davidpark48@hotmail.com>
2024-03-15 12:22:07 -0400

    Updating header to be fixed at the top of the page

--- foodsaver-frontend/src/components/Header/Header.js
+++ foodsaver-frontend/src/components/Header/Header.js
@@ -26,9 +26,10 @@ const Header = ({ setIsLoggedIn }) => {
 
   return(
     <div className="header">
-      <h1 className="header__heading">Food Saver</h1>
-      <img className="header__nav-icon" src={navIcon} alt='nav icon' onClick={() => toggleNav()}/>
- 
+      <div className="header__container">
+        <h1 className="header__heading">Food Saver</h1>
+        <img className="header__nav-icon" src={navIcon} alt='nav icon' onClick={() => toggleNav()}/>
+      </div>
       <Nav 
         isOpen={isNavOpen} 
         closeNavIfLocationMatches={closeNavIfLocationMatches}
