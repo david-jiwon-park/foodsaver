a4126c9b6388f8a46304c63e7067046f2866877e
david-jiwon-park <davidpark48@hotmail.com>
2024-03-17 02:25:33 -0400

    Adding functionality and adding styling for mobile screen sizes for Recipes page

--- foodsaver-frontend/src/pages/RecipesPage/RecipesPage.js
+++ foodsaver-frontend/src/pages/RecipesPage/RecipesPage.js
@@ -1,5 +1,7 @@
+import './RecipesPage.scss';
 import { useEffect, useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 import Header from '../../components/Header/Header';
 import getUserInventory from '../../utils/getUserInventory';
@@ -7,6 +9,9 @@ import getUserInventory from '../../utils/getUserInventory';
 const RecipesPage = ({ isLoggedIn, setIsLoggedIn }) => {
   const navigate = useNavigate();
   const [userInventory, setUserInventory] = useState([]);
+  const [selectedIngredients, setSelectedIngredients] = useState([]);
+  const [suggestedRecipes, setSuggestedRecipes] = useState([]);
+  const [submitted, setSubmitted] = useState(false);
 
   useEffect(() => {
     // Get the user's inventory list if they are logged in 
@@ -16,30 +21,86 @@ const RecipesPage = ({ isLoggedIn, setIsLoggedIn }) => {
       navigate('/');
     }
     }, [isLoggedIn]);
+
+  const handleIngredientToggle = (ingredient) => {
+    if (selectedIngredients.includes(ingredient)) {
+      setSelectedIngredients(selectedIngredients.filter(item => item !== ingredient));
+    } else {
+      setSelectedIngredients([...selectedIngredients, ingredient]);
+    }
+  };
+
+
+  const findRecipes = (e) => {
+    e.preventDefault();
+    const apiURL = 'https://api.edamam.com/api/recipes/v2?type=public';
+    const appId = '73ad0e04';
+    const appKey = '5285fad8f8d27d8157cd7e13a79df213';	
+    axios
+    .get(`${apiURL}&q=${selectedIngredients.join('+')}&app_id=${appId}&app_key=%20${appKey}`)
+    .then((response) => {
+      setSubmitted(true);
+      setSuggestedRecipes(response.data.hits);
+      console.log(selectedIngredients);
+      console.log(response.data.hits);
+    })
+    .catch((error) => {
+      setSubmitted(true);
+      console.log(error);
+    });
+  };
     
   return (
-    <div>
+    <>
       <Header 
         setIsLoggedIn={setIsLoggedIn}
       />
-      <h1>Recipes Page</h1>
-      <h2>Inventory</h2>
-      {userInventory.filter((item) => {
-        return item.discarded == 0
-      })
-      .map((item) => (
-      <div key={item.id}>
-        <h3>{item.food_item}</h3>
+      <div className="recipes-page">
+        <h1 className="recipes-page__heading">Recipes</h1>
+        <h2 className="recipes-page__subheading">Inventory</h2>
+
+        <form onSubmit={findRecipes}>
+          <div className="recipes-page__inventory-outer-container">
+            <div className="recipes-page__inventory-inner-container">
+              {userInventory.filter((item) => {
+                return item.discarded === 0
+              })
+              .map((item) => (
+              <button 
+                type="button"
+                key={item.id}
+                onClick={() => handleIngredientToggle(item.food_item)}
+                className= {selectedIngredients.includes(item.food_item) ? "recipes-page__button-inventory recipes-page__button-inventory--selected" : "recipes-page__button-inventory"}
+              >
+                {item.food_item}
+              </button>
+              ))}
+            </div>
+          </div>
+          <div className="recipes-page__find-button-container">
+            <button type="submit" className="recipes-page__find-button">Find Recipes</button>
+          </div>
+        </form>
+        
+        {submitted ? 
+        (<>
+          <h2 className="recipes-page__subheading recipes-page__subheading--recipes">Suggested Recipes</h2>
+          <div className="recipes-page__recipes-outer-container">
+            <div className="recipes-page__recipes-inner-container">
+              {suggestedRecipes.map((recipe) => (
+              <div key={recipe.recipe.uri} className="recipes-page__recipe-container">
+                <img className="recipes-page__recipe-image" src={recipe.recipe.image} alt="recipe"/>
+                <p className="recipes-page__recipe-name">{recipe.recipe.label}</p>
+              </div>
+              ))}
+            </div>
+          </div>
+        </>
+        )
+        : null }
+
       </div>
-      ))}
-
-      <button>Find Recipes</button>
-
-      <h2>Suggested Recipes</h2>
-
-      <h2>Favorite Recipes</h2>
-
-    </div>
+    </>
   )
 };
 
