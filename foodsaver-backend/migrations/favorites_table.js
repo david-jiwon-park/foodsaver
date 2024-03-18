e056912a8ef1468c0d76a9336cd2f613537827e1
david-jiwon-park <davidpark48@hotmail.com>
2024-03-17 22:37:27 -0400

    Updating favorites table in database

--- foodsaver-backend/migrations/favorites_table.js
+++ foodsaver-backend/migrations/favorites_table.js
@@ -1,17 +1,16 @@
 exports.up = function (knex) {
-    return knex.schema.createTable('favorites', (table) => {
-      table.increments('id').primary();
-      table.integer('recipe_id').notNullable();
-      table
-        .integer('user_id')
-        .unsigned()
-        .references('users.id')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE');
-    });
-  };
-  
-exports.down = function (knex) {
-  return knex.schema.dropTable('favorites');
+  return knex.schema.createTable('favorites', (table) => {
+    table.increments('id').primary();
+    table.string('recipe_uri').notNullable();
+    table
+      .integer('user_id')
+      .unsigned()
+      .references('users.id')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
 };
-  
+
+exports.down = function (knex) {
+return knex.schema.dropTable('favorites');
+};
