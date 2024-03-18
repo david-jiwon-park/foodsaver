10911f17e5be525bf493013ecd5fc31f7ffa4b2c
david-jiwon-park <davidpark48@hotmail.com>
2024-03-17 20:51:09 -0400

    Adding nutritional info and cooking instructions link in recipe modal

--- foodsaver-frontend/src/components/RecipeModal/RecipeModal.js
+++ foodsaver-frontend/src/components/RecipeModal/RecipeModal.js
@@ -31,33 +31,74 @@ const RecipeModal = ({ isOpen, onClose, uri, image, name, servings, ingredients,
 
         <section className="recipe-modal__nutrition-section">
           <h4 className="recipe-modal__section-heading">Nutritional Information</h4>
-          <div>
-            <div className="recipe-modal__macros">
-              <div className="recipe-modal__macro-container">
-                <p>PROTEIN</p>
-                <p>{Math.floor(nutrition["PROCNT"].quantity)}{nutrition["PROCNT"].unit}</p>
-
+          <div className="recipe-modal__nutrition-content">
+            
+            <div className="recipe-modal__top-row"> 
+              <div className="recipe-modal__macros">
+                <div className="recipe-modal__macro-micro-container">
+                  <div className="recipe-modal__circle recipe-modal__circle--protein"></div>
+                  <p className="recipe-modal__macro-micro">PROTEIN</p>
+                  <p className="recipe-modal__macro-micro recipe-modal__macro-micro--quantity">{Math.floor(nutrition["PROCNT"].quantity)}{nutrition["PROCNT"].unit}</p>
+                </div>
+                <div className="recipe-modal__macro-micro-container">
+                  <div className="recipe-modal__circle recipe-modal__circle--fat"></div>
+                  <p className="recipe-modal__macro-micro">FAT</p>
+                  <p className="recipe-modal__macro-micro recipe-modal__macro-micro--quantity">{Math.floor(nutrition["FAT"].quantity)}{nutrition["FAT"].unit}</p>
+                </div>
+                <div className="recipe-modal__macro-micro-container">
+                  <div className="recipe-modal__circle recipe-modal__circle--carb"></div>
+                  <p className="recipe-modal__macro-micro">CARB</p>
+                  <p className="recipe-modal__macro-micro recipe-modal__macro-micro--quantity">{Math.floor(nutrition["CHOCDF"].quantity)}{nutrition["CHOCDF"].unit}</p>
+                </div>
               </div>
-              <div>
-                <p>FAT</p>
-                <p>{Math.floor(nutrition["FAT"].quantity)}{nutrition["FAT"].unit}</p>
-              </div>
-              <div>
-                <p>CARB</p>
-                <p>{Math.floor(nutrition["CHOCDF"].quantity)}{nutrition["CHOCDF"].unit}</p>
-              </div>
+            </div>
               
-
+            <div className="recipe-modal__bottom-row">
+              <div className="recipe-modal__kcal-container">
+                <div className="recipe-modal__kcal-border">
+                  <p className="recipe-modal__kcal recipe-modal__kcal--bold">{Math.floor(nutrition["ENERC_KCAL"].quantity/servings)} kcal</p>
+                  <p className="recipe-modal__kcal">per serving</p>
+                </div>
+              </div>
+              <div className="recipe-modal__micro-container">
+                <div className="recipe-modal__macro-micro-container">
+                  <p className="recipe-modal__macro-micro">Cholesterol</p>
+                  <p className="recipe-modal__macro-micro recipe-modal__macro-micro--quantity">{Math.floor(nutrition["CHOLE"].quantity/servings)}{nutrition["CHOLE"].unit}</p>
+                </div>
+                <div className="recipe-modal__macro-micro-container">
+                  <p className="recipe-modal__macro-micro">Sodium</p>
+                  <p className="recipe-modal__macro-micro recipe-modal__macro-micro--quantity">{Math.floor(nutrition["NA"].quantity/servings)}{nutrition["NA"].unit}</p>
+                </div>
+                <div className="recipe-modal__macro-micro-container">
+                  <p className="recipe-modal__macro-micro">Calcium</p>
+                  <p className="recipe-modal__macro-micro recipe-modal__macro-micro--quantity">{Math.floor(nutrition["CA"].quantity/servings)}{nutrition["CA"].unit}</p>
+                </div>
+                <div className="recipe-modal__macro-micro-container">
+                  <p className="recipe-modal__macro-micro">Magnesium</p>
+                  <p className="recipe-modal__macro-micro recipe-modal__macro-micro--quantity">{Math.floor(nutrition["MG"].quantity/servings)}{nutrition["MG"].unit}</p>
+                </div>
+                <div className="recipe-modal__macro-micro-container">
+                  <p className="recipe-modal__macro-micro">Potassium</p>
+                  <p className="recipe-modal__macro-micro recipe-modal__macro-micro--quantity">{Math.floor(nutrition["K"].quantity/servings)}{nutrition["K"].unit}</p>
+                </div>
+                <div className="recipe-modal__macro-micro-container">
+                  <p className="recipe-modal__macro-micro">Iron</p>
+                  <p className="recipe-modal__macro-micro recipe-modal__macro-micro--quantity">{Math.floor(nutrition["FE"].quantity/servings)}{nutrition["FE"].unit}</p>
+                </div>
+              </div>
             </div>
           </div>
         </section>
 
         <section className="recipe-modal__instructions-section">
           <h4 className="recipe-modal__section-heading">Cooking Instructions</h4>
+          <p className="recipe-modal__instructions-text">For cooking instructions and more details about this recipe, click 
+            <a className="recipe-modal__instructions-link" href={directionsLink} target="_blank"> here!</a>
+          </p>
         </section>
+
       </div>
     </div>
-      
     );
   };
   
